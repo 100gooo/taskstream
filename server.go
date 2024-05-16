f0a3cfcc17ad9555931dc14c34d3026297173842
100gooo <163338515+100gooo@users.noreply.github.com>
2024-05-16 10:57:58 +0300

    optimize the naming

--- server.go
+++ server.go
@@ -1,184 +1,66 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"sync"
-	"time"
+    "context"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "sync"
+    "time"
 
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+    "github.com/gorilla/mux"
+    "github.com/joho/godotenv"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Task struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty"`
-	Title       string             `bson:"title,omitempty"`
-	Description string             `bson:"description,omitempty"`
-	Done        bool               `bson:"done,omitempty"`
+    ID          primitive.ObjectID `bson:"_id,omitempty"`
+    Title       string             `bson:"title,omitempty"`
+    Description string             `bson:"description,omitempty"`
+    Done        bool               `bson:"done,omitempty"`
 }
 
 var client *mongo.Client
 var once sync.Once
 
 func connectToMongo() *mongo.Client {
-	once.Do(func() {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file: ", err)
-		}
-		mongoURI := os.Getenv("MONGO_URI")
+    once.Do(func() {
+        err := godotenv.Load()
+        if err != nil {
+            log.Fatal("Error loading .env file: ", err)
+        }
+        mongoURI := os.Getenv("MONGO_URI")
 
-		clientOptions := options.Client().ApplyURI(mongoURI)
-		var errConnect error
-		client, errConnect = mongo.Connect(context.TODO(), clientOptions)
-		if errConnect != nil {
-			log.Fatal("Error connecting to MongoDB: ", errConnect)
-		}
+        clientOptions := options.Client().ApplyURI(mongoURI)
+        var errConnect error
+        client, errConnect = mongo.Connect(context.Background(), clientOptions) // Adjusted for context.Background()
+        if errConnect != nil {
+            log.Fatal("Error connecting to MongoDB: ", errConnect)
+        }
 
-		errPing := client.Ping(context.TODO(), nil)
-		if errPing != nil {
-			log.Fatal("Failed to connect to MongoDB: ", errPing)
-		}
+        errPing := client.Ping(context.Background(), nil) // Adjusted for context.Background()
+        if errPing != nil {
+            log.Fatal("Failed to connect to MongoDB: ", errPing)
+        }
 
-		fmt.Println("Connected to MongoDB!")
-	})
-	return client
-}
-
-func CreateTask(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var task Task
-
-	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		log.Printf("Error decoding task: %v", err)
-		http.Error(w, "Error decoding task", http.StatusBadRequest)
-		return
-	}
-
-	collection := client.Database("goTasks").Collection("tasks")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	result, err := collection.InsertOne(ctx, task)
-	if err != nil {
-		log.Printf("Error inserting task: %v", err)
-		http.Error(w, "Error inserting task", http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(result)
-}
-
-func GetTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var tasks []Task
-	collection := client.Database("goTasks").Collection("tasks")
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	queryParams := r.URL.Query()
-	title := queryParams.Get("title")
-
-	var filter bson.M
-	if title != "" {
-		filter = bson.M{"title": bson.M{"$regex": primitive.Regex{Pattern: title, Options: "i"}}}
-	} else {
-		filter = bson.M{}
-	}
-
-	cursor, err := collection.Find(ctx, filter)
-	if err != nil {
-		log.Printf("Error finding tasks: %v", err)
-		http.Error(w, "Error finding tasks", http.StatusInternalServerError)
-		return
-	}
-	defer cursor.Close(ctx)
-	for cursor.Next(ctx) {
-		var task Task
-		if err = cursor.Decode(&task); err != nil {
-			log.Printf("Error decoding task: %v", err)
-			http.Error(w, "Error decoding task", http.StatusInternalServerError)
-			return
-		}
-		tasks = append(tasks, task)
-	}
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func UpdateTask(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	id, err := primitive.ObjectIDFromHex(params["id"])
-	if err != nil {
-		log.Printf("Invalid task ID: %v", err)
-		http.Error(w, "Invalid task ID", http.StatusBadRequest)
-		return
-	}
-
-	var task Task
-	if err = json.NewDecoder(r.Body).Decode(&task); err != nil {
-		log.Printf("Error decoding task: %v", err)
-		http.Error(w, "Error decoding task", http.StatusBadRequest)
-		return
-	}
-
-	collection := client.Database("goTasks").Collection("tasks")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	result, err := collection.UpdateOne(
-		ctx,
-		bson.M{"_id": id},
-		bson.D{
-			{"$set", bson.D{{"title", task.Title}, {"description", task.Description}, {"done", task.Done}}},
-		},
-	)
-	if err != nil {
-		log.Printf("Error updating task: %v", err)
-		http.Error(w, "Error updating task", http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(result)
-}
-
-func DeleteTask(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	id, err := primitive.ObjectIDFromHex(params["id"])
-	if err != nil {
-		log.Printf("Invalid task ID: %v", err)
-		http.Error(w, "Invalid task ID", http.StatusBadRequest)
-		return
-	}
-
-	collection := client.Database("goTasks").Collection("tasks")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	result, err := collection.DeleteOne(ctx, bson.M{"_id": id})
-	if err != nil {
-		log.Printf("Error deleting task: %v", err)
-		http.Error(w, "Error deleting task", http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(result)
+        fmt.Println("Connected to MongoDB!")
+    })
+    return client
 }
 
 func main() {
-	client = connectToMongo()
-	r := mux.NewRouter()
+    client = connectToMongo()
+    r := mux.NewRouter()
 
-	r.HandleFunc("/api/tasks", CreateTask).Methods("POST")
-	r.HandleFunc("/api/tasks", GetTasks).Methods("GET")
-	r.HandleFunc("/api/tasks/{id}", UpdateTask).Methods("PUT")
-	r.HandleFunc("/api/tasks/{id}", DeleteTask).Methods("DELETE")
+    r.HandleFunc("/api/tasks", CreateTask).Methods("POST")
+    r.HandleFunc("/api/tasks", GetTasks).Methods("GET")
+    r.HandleFunc("/api/tasks/{id}", UpdateTask).Methods("PUT")
+    r.HandleFunc("/api/tasks/{id}", DeleteTask).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
